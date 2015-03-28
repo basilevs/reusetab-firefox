28aaeacfaa83923842f9e5f1c444ecd0489321ea
Vasili Gulevich <V.V.Gulevich@mail.ru>
2015-03-28 21:46:32 +0600

    Fixed closing on entering a new address on about:newtab A significant refactoring is done improving comments, names, modularity. Fixes #4.

--- test/test-utils.js
+++ test/test-utils.js
@@ -26,26 +26,80 @@ exports["test blockAfterSuccess"] = function(assert) {
 };
 
 exports["test expireAfter"] = function(assert, done) {
-	assert.pass("async Unit test running!");
 	var i = 0;
 	function delegate() {
 		return i++;
 	}
-	var wrapped = utils.expireAfter(delegate, 100);	
+	var wrapped = utils.expireAfter(delegate, Date.now()+100);
 	assert.equal(0, i, "Delegate is not called during wrapping");
 	wrapped();
 	assert.equal(1, i, "Delegate is called immediately after creation");
 	
 	timers.setTimeout(function() {
 		wrapped();
-		assert.equal(2, i, "Delegate is called after 10 ms delay");
+		assert.equal(2, i, "Delegate is called before timeout 50 ms delay");
 	}, 50);
 	timers.setTimeout(function() {
 		assert.equal(2, i, "Previous timer executed");
 		wrapped();
-		assert.equal(2, i, "Delegate is not called after 200 ms delay");
+		assert.equal(2, i, "Delegate is not called after last call + timeout ");
 		done();
-	}, 200);
+	}, 120);
+};
+
+exports["test expireAfterInactivity"] = function(assert, done) {
+	var i = 0;
+	function delegate() {
+		return i++;
+	}
+	var wrapped = utils.expireAfterInactivity(delegate, 100);
+	assert.equal(0, i, "Delegate is not called during wrapping");
+	wrapped();
+	assert.equal(1, i, "Delegate is called immediately after creation");
+	
+	timers.setTimeout(function() {
+		wrapped();
+		assert.equal(2, i, "Delegate is called before timeout");
+	}, 50);
+	timers.setTimeout(function() {
+		wrapped();
+		assert.equal(3, i, "Delegate is called after timeout but before last call + timeout");
+	}, 120);
+	timers.setTimeout(function() {
+		assert.equal(3, i, "Previous timer executed");
+		wrapped();
+		assert.equal(3, i, "Delegate is not called after last call + timeout ");
+		done();
+	}, 250);
+
+};
+
+exports["test expireAfterInactivity2"] = function(assert, done) {
+	var i = 0;
+	function delegate() {
+		return i++;
+	}
+	var wrapped = utils.expireAfterInactivity(delegate, 100);
+	timers.setTimeout(function() {
+		assert.equal(0, i, "Delegate is not called during creation");
+		wrapped();
+		assert.equal(1, i, "Delegate is called after creation + timeout");
+		done();
+	}, 120);
+};
+
+
+
+exports["test areUrlsEqualByHost"] = function(assert) {
+	var areEqual = utils.areUrlsEqualByHost;
+	function assertEqual(url1, url2) {
+		assert.ok(areEqual(url1, url2), url1 + " == " + url2);
+	}
+	function assertNotEqual(url1, url2) {
+		assert.ok(!areEqual(url1, url2), url1 + " != " + url2);
+	}
+	assertEqual("https://git.eclipse.org/r/#/q/is:watched+status:open", "https://git.eclipse.org/r/44335");
+	assertNotEqual("about:blank", "about:home");
 };
 
 require("sdk/test").run(exports);
