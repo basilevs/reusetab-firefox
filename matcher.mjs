d878143f29e9e9637608cd46e44e3b00cbf89347
Vasili Gulevich <vasili.gulevich@xored.com>
2020-01-21 11:48:46 +0700

    Do not reuse a different slack workspace

--- matcher.mjs
+++ matcher.mjs
@@ -12,7 +12,8 @@ function checkTab(tab) {
 }
 
 const defaultConfiguration = [
-    'https://mail\\.google\\.com/mail/u/(\d+).*'
+    'https://app.slack.com/client/([\dA-Z]+)/.*',
+    'https://mail\\.google\\.com/mail/u/(\d+).*',
     'https://(?:www\\.spotify\\.com|www\\.open\\.spotify\\.com)/.*',
     'https://www\\.google\\.com/maps.*',
     'https://([^/]+)/.*',
